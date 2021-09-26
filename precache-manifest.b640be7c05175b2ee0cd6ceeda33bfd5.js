7639a4b4a32dad3d053ae1846a0e13d6cf3667f7
StelPan <20step01@gmail.com>
2021-09-26 18:31:15 +0700

    Initial commit

---
+++ precache-manifest.b640be7c05175b2ee0cd6ceeda33bfd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8b59a3c2493e5f8489",
+    "url": "/deploy-github-crypto/css/app.0e433876.css"
+  },
+  {
+    "revision": "c7e10b2ed70e1ed363d5",
+    "url": "/deploy-github-crypto/css/chunk-vendors.c7db8543.css"
+  },
+  {
+    "revision": "556a5337dd08ea66881674f72bd4c91d",
+    "url": "/deploy-github-crypto/index.html"
+  },
+  {
+    "revision": "ce8b59a3c2493e5f8489",
+    "url": "/deploy-github-crypto/js/app.0ac1e794.js"
+  },
+  {
+    "revision": "c7e10b2ed70e1ed363d5",
+    "url": "/deploy-github-crypto/js/chunk-vendors.547863a8.js"
+  },
+  {
+    "revision": "732662f22f23e3a6a8ddeacee4a93604",
+    "url": "/deploy-github-crypto/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deploy-github-crypto/robots.txt"
+  }
+]);
